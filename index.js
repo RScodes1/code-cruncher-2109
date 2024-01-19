0cd334b68d25575b6bc8ea983d743badb8e095d8
rajcodes168h <raj19all@gmail.com>
2024-01-19 19:30:12 +0530

    added navbar-updated

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let ColoringUrl = `https://mock-final-copy-api.onrender.com/images`;
+let AffirmUrl = `https://mock-final-copy-api.onrender.com/affirmations`;
+let ContactUrl = `https://mock-final-copy-api.onrender.com/contacts`;
+let ThoughtUrl = `https://mock-final-copy-api.onrender.com/thoughts`;
+
+let colorData = [];
+
+
