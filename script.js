ddf68271b3a5e4899a16250a2d37c124a7b20c4b
Abhaysinh Gaikwad <abhaygaikwad7666@gmail.com>
2024-01-19 19:38:58 +0530

    FAQ About Privacy html css  added

--- script.js
+++ script.js
@@ -21,3 +21,38 @@ function arrowFunction(e, id) {
   }
   console.log(e.target.dataset.id);
 }
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+let contactSubmitBtn = document.getElementById("contactSubmitBtn");
+
+async function addContact() {
+  let contact = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    message: message.value,
+  };
+  console.log(contact);
+
+  let res = await fetch("https://mock-final-copy-api.onrender.com/Contacts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(contact),
+  });
+  let data = await res.json();
+  console.log(data);
+}
+
+contactSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addContact();
+  //   console.log(firstName.value);
+  //   console.log(lastName.value);
+  //   console.log(email.value);
+  //   console.log(message.value);
+});
